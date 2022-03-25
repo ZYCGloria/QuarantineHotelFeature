3fcbf5caf56214e1bad6ed74b5b4cccf70ae6e71
ZYCGloria <75005192+ZYCGloria@users.noreply.github.com>
2022-03-25 10:08:45 +0800

    Update ForeignBrandPie.js

--- js/ForeignBrandPie.js
+++ js/ForeignBrandPie.js
@@ -77,8 +77,8 @@ am4core.ready(function() {
       return series;
     }
     
-    createSeries("europe", "Healthcare Expenditure");
-    createSeries("namerica", "Other Public Health Expenditure");
+    createSeries("europe", "Public Health Expenditure");
+    createSeries("namerica", "Other Healthcare Expenditure");
     
     var title = chart.titles.create();
     title.text = "Surging Healthcare Expenditure due to Covid";
@@ -91,75 +91,3 @@ am4core.ready(function() {
     chart.legend.position = "top";
     
     }); // end am4core.ready()
-
-// am4core.ready(function() {
-
-//   // Themes begin
-//   am4core.useTheme(am4themes_animated);
-//   // Themes end
-  
-//   var chart = am4core.create("chartdiv5", am4charts.XYChart);
-  
-//   chart.data = [{
-//           "year": "2017",
-//           "income": 1445.1,
-//           "expenses": 188.6
-//       }, {
-//           "year": "2018",
-//           "income": 1562.4,
-//           "expenses": 203.9
-//       }, {
-//           "year": "2019",
-//           "income": 1666.5,
-//           "expenses": 221.2
-//       }, {
-//           "year": "2020",
-//           "income": 1920.1,
-//           "expenses": 387.9
-//       }, {
-//           "year": "2021",
-//           "income": 1920.5,
-//           "expenses": 397.8
-//       }];
-  
-//   //create category axis for years
-//   var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-//   categoryAxis.dataFields.category = "year";
-//   categoryAxis.renderer.inversed = true;
-//   categoryAxis.renderer.grid.template.location = 0;
-  
-//   //create value axis for income and expenses
-//   var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-//   valueAxis.renderer.opposite = true;
-  
-  
-//   //create columns
-//   var series = chart.series.push(new am4charts.ColumnSeries());
-//   series.dataFields.categoryY = "year";
-//   series.dataFields.valueX = "income";
-//   series.name = "Healthcare Expenditure";
-//   series.columns.template.fillOpacity = 0.5;
-//   series.columns.template.strokeOpacity = 0;
-//   series.tooltipText = "Healthcare Expenditure in {categoryY}: {valueX.value}";
-  
-//   //create line
-//   var lineSeries = chart.series.push(new am4charts.LineSeries());
-//   lineSeries.dataFields.categoryY = "year";
-//   lineSeries.dataFields.valueX = "expenses";
-//   lineSeries.name = "Public Health Expenditure";
-//   lineSeries.strokeWidth = 3;
-//   lineSeries.tooltipText = "Public Health Expenditure in {categoryY}: {valueX.value}";
-  
-//   //add bullets
-//   var circleBullet = lineSeries.bullets.push(new am4charts.CircleBullet());
-//   circleBullet.circle.fill = am4core.color("#fff");
-//   circleBullet.circle.strokeWidth = 2;
-  
-//   //add chart cursor
-//   chart.cursor = new am4charts.XYCursor();
-//   chart.cursor.behavior = "zoomY";
-  
-//   //add legend
-//   chart.legend = new am4charts.Legend();
-  
-//   }); // end am4core.ready()
